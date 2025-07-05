17ba52e23d3f026612c89abaad689e66abcc1c83
Rehan Butt <naher94@users.noreply.github.com>
2025-07-05 12:22:10 -0700

    Flip Clock Updated

--- js/app.js
+++ js/app.js
@@ -1,1 +1,6 @@
 // $(document).foundation();
+
+// For flip clock footer items
+const footer = document.querySelector('footer');
+const flipCardWidth = document.querySelector('.static-number').getBoundingClientRect().width;
+footer.style.setProperty('--flip-card-width', `${flipCardWidth}px`);
