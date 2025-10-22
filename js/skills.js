4012423d405de418aed0245412a52b4b4def9c25
Rehan Butt <naher94@users.noreply.github.com>
2025-10-21 21:48:12 -0700

    Animation Update

--- js/skills.js
+++ js/skills.js
@@ -2,12 +2,12 @@ const boxes = document.querySelectorAll('.skill');
 async function animateBox(box) {
   // Move left
   box.style.transform = 'translateX(calc(calc(1.25em + var(--skill-padding))* -1))';
-  await new Promise(r => setTimeout(r, 2000));
+  await new Promise(r => setTimeout(r, 3000));
   // Slide back
   box.style.transform = 'translateX(0)';
   await new Promise(r => setTimeout(r, 500));
   // Wait before next
-  await new Promise(r => setTimeout(r, 2000));
+  await new Promise(r => setTimeout(r, 3000));
 }
 async function loop() {
   while (true) {
